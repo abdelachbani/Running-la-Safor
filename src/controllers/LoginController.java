a9099ff01411e8953430d4119785035aef146ffa
Abdel <abdelkarimachbaniabid724@gmail.com>
2026-05-18 16:52:49 +0200

    Implemented navigation from login to register.

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,16 +1,24 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.text.Text;
 import upv.ipc.sportlib.SportActivityApp;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 /**
  * FXML Controller class
@@ -42,7 +50,15 @@ public class LoginController implements Initializable {
     }
 
     @FXML
-    private void handleRegisterAction(ActionEvent event) {
+    private void handleRegisterAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Register.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.setMinWidth(400);
+        stage.setMinHeight(500);
+        stage.show();
     }
 
     @FXML
